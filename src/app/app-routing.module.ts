8cc474726461fe74e0208af1423daca68747b6fb
biancusquis <114453235+biancusquis@users.noreply.github.com>
2023-11-23 13:11:10 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InvitadoTriviaComponent } from './invitado-trivia/invitado-trivia.component';
+import { QrCodeComponent } from './qr-code/qr-code.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InvitadoTriviaComponent }, // Página principal
+  { path: 'qr-code', component: QrCodeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
